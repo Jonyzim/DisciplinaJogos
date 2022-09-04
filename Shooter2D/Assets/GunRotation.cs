180116160af19f7559b90febd4dba9e47cb1b10c
Jony33Invent <35571614+Jony33Invent@users.noreply.github.com>
2022-09-04 20:12:47 -0300

    Teste de rotação da arma

---
+++ Shooter2D/Assets/GunRotation.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotation : MonoBehaviour
+{
+    Camera cam;
+    [SerializeField]SpriteRenderer bodySprite;
+    [SerializeField] SpriteRenderer gunSprite;
+    private Vector3 direction;
+    private Quaternion lookRotation;
+    float angle;
+
+    [SerializeField] private float rotationSpeed;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 center = transform.position;
+
+        direction = (mousePos - center).normalized;
+
+        if (direction.x > 0) // virado pra direita
+        {
+            transform.parent.localScale = new Vector3(1, 1, 1);
+            angle = -Vector2.SignedAngle(direction,  Vector2.right);
+        }
+        else // virado pra esquerda
+        {
+            transform.parent.localScale = new Vector3(-1, 1, 1);
+            angle = -Vector2.SignedAngle(direction, Vector2.left);
+        }
+
+        lookRotation = Quaternion.Euler(0,0,angle);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+    }
+}
---
+++ Shooter2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float _x= Input.GetAxisRaw("Horizontal");
+        float _y = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(_x, _y, 0)*speed;
+    }
+}
