58abe4e21980feffdf9001a26e031a2e535a33ea
Felipe Cadavez Oliveira <fecadavez@usp.br>
2022-09-17 15:49:13 -0300

    reload funfando na gambiarra

--- Shooter2D/Assets/Scripts/Guns/Gun.cs
+++ Shooter2D/Assets/Scripts/Guns/Gun.cs
@@ -53,10 +53,12 @@ public abstract class Gun : MonoBehaviour
 
     public void SetOwner(Character character){
         character.onFire += Fire;
+        character.onReload += Teste;
     }
 
     public void RemoveOwner(Character character){
         character.onFire -= Fire;
+        character.onReload -= Teste;
     }
 
     protected virtual void Start()
@@ -77,20 +79,27 @@ public abstract class Gun : MonoBehaviour
     {
 
         if(cur_magazine == 0 && cd <= 0){
-            ReloadProps(reloadTime);
-            reloadProgress += Time.deltaTime;
-            GameEvents.current.ReloadUpdate(1, reloadProgress/reloadTime);
-            if(reloadProgress > reloadTime){
-                reloadProgress = 0;
-                cur_magazine = magazine;
-                GameEvents.current.ReloadUpdate(1, 0);
-                GameEvents.current.MagazineUpdate(1, (float)cur_magazine/(float)magazine);
-            }
+            Reload();
         }
         else {
             reloadProgress = 0;
         }
-
         cd -= Time.deltaTime;        
+    }
+
+    void Reload(){
+        ReloadProps(reloadTime);
+        reloadProgress += Time.deltaTime;
+        GameEvents.current.ReloadUpdate(1, reloadProgress/reloadTime);
+        if(reloadProgress > reloadTime){
+            reloadProgress = 0;
+            cur_magazine = magazine;
+            GameEvents.current.ReloadUpdate(1, 0);
+            GameEvents.current.MagazineUpdate(1, (float)cur_magazine/(float)magazine);
+        }
+    }
+
+    void Teste(){
+        cur_magazine = 0;
     }
 }
