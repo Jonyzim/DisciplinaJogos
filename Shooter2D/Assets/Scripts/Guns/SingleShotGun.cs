5d85344e9e9f41460c9b42df9dbecab4f011ee1d
pliduino <liduino@usp.br>
2022-09-20 14:28:58 -0300

    Update SingleShotGun.cs

--- Shooter2D/Assets/Scripts/Guns/SingleShotGun.cs
+++ Shooter2D/Assets/Scripts/Guns/SingleShotGun.cs
@@ -13,7 +13,7 @@ public abstract class SingleShotGun : Gun
             base.Fire(direction);
             Vector3 _direction = Quaternion.AngleAxis(-Random.Range(-spread, spread), new Vector3(0, 0, 1)) * direction;
             
-            GameObject _bullet = Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, 0));
+            GameObject _bullet = Instantiate(bullet, spawnTransf.transform.position, Quaternion.Euler(0, 0, 0));
             Bullet bulletScript = _bullet.GetComponent<Bullet>();
             bulletScript.SetPlayer(player);
             bulletScript.SetDirection(_direction);
