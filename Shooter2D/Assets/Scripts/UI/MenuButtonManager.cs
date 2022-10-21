a27ff56946583fd1748f9e691ad92ac9c2e1d391
Felipe Cadavez Oliveira <fecadavez@usp.br>
2022-10-20 22:44:42 -0300

    Menu atualizado

--- Shooter2D/Assets/Scripts/UI/MenuButtonManager.cs
+++ Shooter2D/Assets/Scripts/UI/MenuButtonManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MenuButtonManager : MonoBehaviour
 {
     [SerializeField] GameObject menuPanel;
+    // [SerializeField] GameObject Menu;
     private void Start()
     {
         Cursor.visible = true;
